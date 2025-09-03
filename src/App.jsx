b205e10d6ed268a625dc8c229c65059384853dd2
DamianoMura <mura.damiano14@gmail.com>
2025-09-03 14:39:51 +0200

    [upd] preparing endpoints and state variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../components/Header'
 
-function App() {
 
+//actresses endpoint 
+ const actressesUrl= "https://lanciweb.github.io/demo/api/actresses/";
+//actors endpoint
+ const actorsUrl="https://lanciweb.github.io/demo/api/actors/";
+function App() {
+//setting state variable for actors
+const [actors,setActors]=useState([]);
+//setting state variable for actresses
+const [actress,setActress]=useState([]);
 
   return (
     <>
